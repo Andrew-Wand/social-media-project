d26afda9b39dfbbbad942cdc628d0624deb180d7
Andrew-Wand <andrewwands@gmail.com>
2024-04-11 21:27:25 -0500

    style: all posts tabe now has css for mobile

--- frontend/src/components/AllPosts.jsx
+++ frontend/src/components/AllPosts.jsx
@@ -4,6 +4,7 @@ import UserService from "../services/user.service";
 import PostService from "../services/post.service";
 import { Link, useNavigate } from "react-router-dom";
 import { HiMiniHeart, HiOutlineHeart } from "react-icons/hi2";
+import moment from "moment";
 
 const AllPosts = () => {
   const [currentUser, setCurrentUser] = useState(undefined);
@@ -117,74 +118,54 @@ const AllPosts = () => {
     <ul>
       {allPosts?.map((post, i) => (
         <li
-          className="bg-slate-500 text-black my-5 p-10 mx-5 rounded-lg shadow-lg first:mt-3"
+          className="bg-transparent text-white xl:mx-5 w-full first:mt-3"
           key={i}
         >
-          <Link
-            to={`/post/${post.id}`}
-            // onClick={() => fetchPostById(post.id)}
-            className="underline mb-5"
-          >
-            {post.Title}
-          </Link>
-          <p>{post.Text}</p>
-
-          <p>Likes: {post.likes.length}</p>
-          <p>Comments: {post.comments.length}</p>
-
-          <form key={i} value={i} onSubmit={handleCreateLike}>
-            {/* <button
-          className={post.likes.length === 0 ? "btn" : "btn-lg"}
-        >
-          Like
-        </button> */}
-            {/* {post.likes.length > 0 ? (
-              <button
-                key={i}
-                value={i}
-                onClick={(e) => getIndex(e, i)}
-                className=""
-              >
-                <HiMiniHeart className="pointer-events-none text-2xl" />
-              </button>
-            ) : (
-              <button
-                key={i}
-                value={i}
-                onClick={(e) => getIndex(e, i)}
-                className=" "
-              >
-                <HiOutlineHeart className="pointer-events-none text-2xl " />
-              </button>
-            )} */}
-
-            {/* {post.likes?.map((like) => (
-          <div>
-            <button
-              className="btn"
-              onClick={() => console.log(post.likes)}
-            >
-              length
-            </button>
-            {post.likes?.length ? "yes" : "no"}
-            <button
-              key={post.id}
-              value={post.id}
-              onClick={() => setKeyIndex(post.id)}
-              className="btn"
-            >
-              Like
-            </button>
-          </div>
-        ))} */}
-          </form>
-
-          <p>
-            Posted By:
-            <Link to={`/profile/${post.userId}`} className="btn">
+          <div className="flex ml-6 mt-3 text-sm">
+            <Link to={`/profile/${post.userId}`} className="link">
               {post.owner}
             </Link>
-          </p>
+            <p className="mx-2">•</p>
+            <p className="">{`${moment(post.createdAt).format("L")}`}</p>
+          </div>
+
+          <div className="text-lg ml-6 my-5">
+            <Link
+              to={`/post/${post.id}`}
+              // onClick={() => fetchPostById(post.id)}
+              className="underline mb-5"
+            >
+              {post.Title}
+            </Link>
+            <p>{post.Text}</p>
+          </div>
+          <div className="flex justify-around mr-10 mb-5">
+            <p className="btn rounded-3xl">Likes: {post.likes.length}</p>
+            <p className="btn rounded-3xl">Comments: {post.comments.length}</p>
+            <form key={i} value={i} onSubmit={handleCreateLike}>
+              {post.likes.length > 0 ? (
+                <button
+                  key={i}
+                  value={i}
+                  onClick={(e) => getIndex(e, i)}
+                  className="btn rounded-full"
+                >
+                  <HiMiniHeart className="pointer-events-none text-2xl" />
+                </button>
+              ) : (
+                <button
+                  key={i}
+                  value={i}
+                  onClick={(e) => getIndex(e, i)}
+                  className="btn rounded-full"
+                >
+                  <HiOutlineHeart className="pointer-events-none text-2xl " />
+                </button>
+              )}
+            </form>
+          </div>
+
+          <hr className="border-b-solid border-b-[.0625rem] border-[#242c2e]" />
         </li>
       ))}
     </ul>
