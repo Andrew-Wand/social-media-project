6397a2de231d8babe9d710915756642e2a5d8786
Andrew-Wand <andrewwands@gmail.com>
2024-06-26 14:36:08 -0500

    feat: now connected to supabase

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,14 +1,28 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.VITE_DATABASE_NAME,
-  process.env.VITE_DATABASE_USER,
-  process.env.VITE_DATABASE_PASSWORD,
-  {
-    dialect: "postgres",
-  }
-);
+// const db_url =
+
+const db_name = "postgres";
+
+const db_user = "postgres.nyghrdxtnpowbwajnqmu";
+
+const db_host = "aws-0-us-east-1.pooler.supabase.com";
+
+const db_password = "M00shym00m001!";
+
+// const sequelize = new Sequelize(
+//   process.env.VITE_DATABASE_NAME,
+//   process.env.VITE_DATABASE_USER,
+//   process.env.VITE_DATABASE_PASSWORD,
+//   {
+//     dialect: "postgres",
+//   }
+// );
+const sequelize = new Sequelize(db_name, db_user, db_password, {
+  dialect: "postgres",
+  host: db_host,
+});
 
 const db = {};
 
